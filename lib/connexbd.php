6f8f3abd7f193981f3148b31f60a897baaf47f24
sanchez <frederic.sf@hotmail.com>
2020-12-15 09:59:53 +0100

    online db

--- lib/connexbd.php
+++ lib/connexbd.php
@@ -1,9 +1,9 @@
 <?php
-    $user = "root";
-    $pass = "";
+    $user = "sql7382175";
+    $pass = "2JfUZmMME8";
 
     try {
-        $pdo = new PDO('mysql:host=localhost;dbname=ecommerce',$user,$pass);
+        $pdo = new PDO('mysql:host=sql7.freemysqlhosting.net;dbname=sql7382175',$user,$pass);
     }
     catch (pdo_exception $e) {
         print "<p>erreur de connexion</p>";
